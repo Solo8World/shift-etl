381380514bf87520641bc38ac161c11b20338c0c
lizhuo <lizhuo@rtmap.com>
2020-08-27 19:05:18 +0800

    init project

---
+++ src/main/java/com/example/api/ShiftEtlApplication.java
@@ -0,0 +1,16 @@
+package com.example.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lizhuo
+ */
+@SpringBootApplication
+public class ShiftEtlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiftEtlApplication.class, args);
+    }
+
+}
